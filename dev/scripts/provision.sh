8d8d7f6bd067385ee280713ae82ec06deaa2436f
Dustin Kerns <Dkerns0123@gmail.com>
2019-09-02 14:24:03 -0700

    Initial commit

---
+++ dev/scripts/provision.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+sudo apt-get install -y ubuntu-mate-desktop \
+    feh \
+    tmux \
+    git \
+    dconf-editor \
+    ranger
+
+
+# User/System configuration
+
+# install vimrc
+# install blame colors
+# run vim plugin installation
+# Can I set terminal to autohide?
+
+# Python / Pip
+
+# Docker
+
+# Google Chrome
+
+# Ansible
+
+# Desktop Background
