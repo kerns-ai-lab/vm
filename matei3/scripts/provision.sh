c20f502daabec2d8dc6e59378d64afbd7acef740
Dustin Kerns <Dkerns0123@gmail.com>
2019-09-02 17:48:48 -0700

    Clear stored secrets from instance at end of provisioning

--- matei3/scripts/provision.sh
+++ matei3/scripts/provision.sh
@@ -40,5 +40,9 @@ sudo apt-get update
 # Required dependencies for running ansible and cloning repository
 sudo apt-get install -y python-pip git
 git clone https://${git_user}:${git_pass}@github.com/kerns-ai-lab/vm.git /home/vagrant/vm
-mv /home/vagrant/vm/ansible /home/vagrant/
-
+# Copy ansible folder from cloned repo to conventional directory 
+cp -r /home/vagrant/vm/ansible /home/vagrant/ansible
+# Remove stored credentials from system file
+rm /home/vagrant/vm/.git/config
+# Clear bash history
+cat /dev/null > ~/.bash_history && history -c
